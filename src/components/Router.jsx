0e2d3224e4770d1870bc1d838a220f0ccddad5bc
Avery Doucet <avery.doucet03@gmail.com>
2022-09-19 01:36:20 -0300

    done

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import Main from '../Page/Main';
 import Groups from '../Page/Groups';
 import Chat from '../Page/Chat';
+import NotFound from '../Page/NotFound';
 
 const Router = () => {
   const location = useLocation();
@@ -12,8 +13,8 @@ const Router = () => {
       <Route exact path='/' element={<Main />} />
       <Route path='/groups/' element={<Groups />} />
       <Route path='/groups/:id' element={<Chat />} />
-      <Route path='/404' element={<Main />} />
-      <Route path='*' element={<Main />} />
+      <Route path='/404' element={<NotFound />} />
+      <Route path='*' element={<Navigate to='/404' />} />
     </Routes>
   );
 };
