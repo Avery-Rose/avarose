5eca344330270148df0cb0e62bb2fa63363aed46
Avery Rose Doucet <avery.doucet03@gmail.com>
2023-03-16 18:13:55 -0300

    seo on main page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,17 @@
 import React, { useEffect } from 'react';
 import ReactGA from 'react-ga4';
+import { seo } from '../Helpers/seo';
 
 const Home = () => {
   useEffect(() => {
     ReactGA.send({
       hitType: 'pageview',
       page: window.location.pathname,
+    });
+
+    seo({
+      title: 'AvaRose',
+      description: "Avery Rose's personal website",
     });
   }, []);
 
